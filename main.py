862c5ea60795c726881d2dd08d76d70dc645c762
NandaEkaR <rostania92@gmail.com>
2021-04-15 15:51:44 +0700

    Biodata Big Data

---
+++ main.py
@@ -0,0 +1,6 @@
+print("Nama     : Nanda Eka Rostania")
+print("NIM      : 04119020")
+print("Prodi    : Sistem Komputer")
+print("Fakultas : Ilmu Komputer")
+print("Kampus   : Universitas Narotama")
+print("Kota     : Surabaya")
